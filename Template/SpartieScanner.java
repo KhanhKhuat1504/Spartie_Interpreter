78e9623b833aff4295a93cfcd026f178c5c4f155
KhanhKhuat1504 <86178396+KhanhKhuat1504@users.noreply.github.com>
2024-10-23 21:19:59 -0400

    Finish Interpreter Part 1

--- Template/SpartieScanner.java
+++ Template/SpartieScanner.java
@@ -37,7 +37,7 @@ public class SpartieScanner {
             if (token.type != TokenType.IGNORE)
                 tokens.add(token);
         }
-        
+
         return tokens;
     }
 
@@ -76,14 +76,14 @@ public class SpartieScanner {
         // Hint: Start of not knowing what the token is, if we can determine it, return
         // it, otherwise, return null
         TokenType type = TokenType.UNDEFINED;
-        
+
         // we do not tokenize spaces and new lines
-        if (nextCharacter == ' ') {
+        if (nextCharacter == ' ' || nextCharacter == '\r' || nextCharacter == '\t') {
             type = TokenType.IGNORE;
         } else if (nextCharacter == '\n') {
             line++;
             type = TokenType.IGNORE;
-        // SIMPLE TOKENS
+            // SIMPLE TOKENS
         } else if (nextCharacter == ';') {
             type = TokenType.SEMICOLON;
         } else if (nextCharacter == ',') {
@@ -99,14 +99,16 @@ public class SpartieScanner {
         } else if (nextCharacter == '+') {
             type = TokenType.ADD;
         } else if (nextCharacter == '-') {
-            type = TokenType.SUBTRACT; 
+            type = TokenType.SUBTRACT;
         } else if (nextCharacter == '*') {
             type = TokenType.MULTIPLY;
+        } else if(nextCharacter == '|') {
+            type = TokenType.OR;
         } else {
             Token token = null;
-            return token;           
+            return token;
         }
-        
+
         current++;
         Token token = new Token(type, String.valueOf(nextCharacter), line);
         return token;
@@ -118,52 +120,54 @@ public class SpartieScanner {
         // Hint: Examine the character for a comparison but check the next character (as
         // long as one is available)
         // For example: < or <=
-        
+
         char nextCharacter = source.charAt(current);
         String text = String.valueOf(nextCharacter);
         TokenType type = TokenType.UNDEFINED;
-        
+
         // check if the first character is a certain comparison; if it is, then examine next character to see if it is of a specific type
         if (nextCharacter == '<') {
             if (current + 1 < source.length() && examine('=')) {
                 text = "<=";
                 type = TokenType.LESS_EQUAL;
-                current++;
+                current+=2;
             } else {
                 type = TokenType.LESS_THAN;
+                current++;
             }
         } else if (nextCharacter == '>') {
             if (current + 1 < source.length() && examine('=')) {
                 text = ">=";
                 type = TokenType.GREATER_EQUAL;
-                current++;
+                current+=2;
             } else {
                 type = TokenType.GREATER_THAN;
+                current++;
             }
         } else if (nextCharacter == '=') {
             if (current + 1 < source.length() && examine('=')) {
                 type = TokenType.EQUIVALENT;
                 text = "==";
-                current++;
+                current+=2;
             } else {
                 type = TokenType.ASSIGN;
+                current++;
             }
         } else if (nextCharacter == '!') {
             if (current + 1 < source.length() && examine('=')) {
                 type = TokenType.NOT_EQUAL;
                 text = "!=";
-                current++;
+                current+=2;
             } else {
                 type = TokenType.NOT;
+                current++;
             }
         } else {
             Token token = null;
-            return token;   
+            return token;
         }
-        
-        Token token = new Token(type, text, line);
-        current++;
 
+        Token token = new Token(type, text, line);
         return token;
     }
 
@@ -176,31 +180,51 @@ public class SpartieScanner {
         if (nextCharacter == '/') {
             if (current + 1 < source.length() && examine('/')) {
                 // ignore entire line after the comment
-                while (current + 1 < source.length() && ! examine('\n')) {
-                    current++;
+                current += 2;
+                while (current < source.length()) {
+                    nextCharacter = source.charAt(current);
+                    if (nextCharacter == '\n') {
+                        line++;
+                        current++;
+                        break;
+                    } else {
+                        current++;
+                    }
                 }
-                type = TokenType.IGNORE;
+                return new Token(TokenType.IGNORE, "", line);
             } else {
-                type = TokenType.DIVIDE;
+                current++;
+                return new Token(TokenType.DIVIDE, "/", line);
             }
-        } else {
-            Token token = null;
-            return token;   
         }
-        Token token = new Token(type, String.valueOf(nextCharacter), line);
-        current++;
-
-        return token;
+        return null;
     }
+
 
     // TODO: Complete implementation
     private Token getStringToken() {
         // Hint: Check if you have a double quote, then keep reading until you hit
         // another double quote
         // But, if you do not hit another double quote, you should report an error
+        if (isAtEnd()) return null;
         char nextCharacter = source.charAt(current);
-
-        String string = null;
+        if (nextCharacter == '"') {
+            current++;
+            int stringStart = current;
+            while (!isAtEnd() && source.charAt(current) != '"') {
+                if (source.charAt(current) == '\n') {
+                    line++;
+                }
+                current++;
+            }
+            if (isAtEnd()) {
+                error(line, "Unterminated string literal.");
+                return null;
+            }
+            String value = source.substring(stringStart, current);
+            current++;
+            return new Token(TokenType.STRING, value, line, value);
+        }
 
         return null;
     }
@@ -209,6 +233,25 @@ public class SpartieScanner {
     private Token getNumericToken() {
         // Hint: Follow similar idea of String, but in this case if it is a digit
         // You should only allow one period in your scanner
+        if (isAtEnd()) return null;
+        char nextCharacter = source.charAt(current);
+        if (isDigit(nextCharacter)) {
+            int numStart = current;
+            boolean hasDecimal = false;
+            while (!isAtEnd() && (isDigit(source.charAt(current)) || source.charAt(current) == '.')) {
+                if (source.charAt(current) == '.') {
+                    if (hasDecimal) {
+                        error(line, "Invalid number format: multiple decimal points.");
+                        return null;
+                    }
+                    hasDecimal = true;
+                }
+                current++;
+            }
+            String numberStr = source.substring(numStart, current);
+            double number = Double.parseDouble(numberStr);
+            return new Token(TokenType.NUMBER, numberStr, line, number);
+        }
         return null;
     }
 
@@ -216,6 +259,20 @@ public class SpartieScanner {
     private Token getIdentifierOrReservedWord() {
         // Hint: Assume first it is an identifier and once you capture it, then check if
         // it is a reserved word.
+        if (isAtEnd()) return null;
+        char nextCharacter = source.charAt(current);
+        if (isAlpha(nextCharacter)) {
+            int idStart = current;
+            while (!isAtEnd() && isAlphaNumeric(source.charAt(current))) {
+                current++;
+            }
+            String text = source.substring(idStart, current);
+            TokenType type = keywords.get(text);
+            if (type == null) {
+                type = TokenType.IDENTIFIER;
+            }
+            return new Token(type, text, line);
+        }
         return null;
     }
 
@@ -227,6 +284,10 @@ public class SpartieScanner {
     private boolean isAlpha(char character) {
         return character >= 'a' && character <= 'z' ||
                 character >= 'A' && character <= 'Z';
+    }
+
+    private boolean isAlphaNumeric(char character) {
+        return isAlpha(character) || isDigit(character) || character == '_';
     }
 
     // This will check if a character is what you expect, if so, it will advance
