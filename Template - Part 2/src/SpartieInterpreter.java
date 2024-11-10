86d5183f2ff6ecded5952da514ec134c4345220c
KhanhKhuat1504 <86178396+KhanhKhuat1504@users.noreply.github.com>
2024-11-10 16:48:21 -0500

    Update SpartieInterpreter.java

--- Template - Part 2/src/SpartieInterpreter.java
+++ Template - Part 2/src/SpartieInterpreter.java
@@ -5,7 +5,8 @@ public class SpartieInterpreter {
     }
 
     Object interpret(Expression expression) {
-        // Depending on the expression type from the parser, we attempt to interpret the expression
+        // Depending on the expression type from the parser, we attempt to interpret the
+        // expression
         return switch (expression) {
             case Expression.LiteralExpression literalExpression -> interpretLiteral(literalExpression);
             case Expression.ParenthesesExpression parenthesesExpression -> interpretParenthesis(parenthesesExpression);
@@ -16,7 +17,8 @@ public class SpartieInterpreter {
     }
 
     private Object interpretLiteral(Expression.LiteralExpression expression) {
-        // This is fairly simple, just return the actual literal value. For example "some string" or 3.0
+        // This is fairly simple, just return the actual literal value. For example
+        // "some string" or 3.0
         return expression.literalValue;
     }
 
@@ -28,16 +30,17 @@ public class SpartieInterpreter {
     private Object interpretUnary(Expression.UnaryExpression expression) {
         Object right = interpret(expression.right);
 
-        // TODO: Complete interpreter. Check the unary operator type (! or -) and perform the operation
-        // TODO: Before negating (-), check to make sure the type is correct using the provided validateOperand method
+        // TODO: Complete interpreter. Check the unary operator type (! or -) and
+        // perform the operation
+        // TODO: Before negating (-), check to make sure the type is correct using the
+        // provided validateOperand method
         switch (expression.operator.type) {
             case NOT:
-                return null;
+                return !isTrue(right);
             case SUBTRACT:
                 validateOperand(expression.operator, right);
-                return null;
+                return -(double) right;
         }
-
         return null;
     }
 
@@ -46,28 +49,41 @@ public class SpartieInterpreter {
         Object right = interpret(expression.right);
 
         // TODO: Add support for String concatenation, for example: "Jane" + "Doe"
-        // TODO: Add support for String concatenation with a double that is rounded, for example "Jane is " + 12
-        // TODO: Handle unique case with add operator that can be applied to Strings and Doubles
+        // TODO: Add support for String concatenation with a double that is rounded, for
+        // example "Jane is " + 12
+        // TODO: Handle unique case with add operator that can be applied to Strings and
+        // Doubles
         if (expression.operator.type == TokenType.ADD) {
             // TODO: Return the correct evaluation
+            if (left instanceof String || right instanceof String) {
+                return left.toString() + right.toString();
+            } else if (left instanceof Double || right instanceof Double) {
+                return (double) left + (double) right;
+            } else {
+                error("Invalid type on line " + expression.operator.line + " : " + expression.operator.text + left
+                        + right);
+            }
         }
 
         // TODO: Test if the two are equivalent or not equivalent
-        switch(expression.operator.type) {
+        switch (expression.operator.type) {
             case EQUIVALENT:
-                return null;
+                return isEquivalent(left, right);
             case NOT_EQUAL:
-                return null;
+                return !isEquivalent(left, right);
         }
 
-        // At this point, we can validate if our operands are doubles because they cannot be Strings for the other
+        // At this point, we can validate if our operands are doubles because they
+        // cannot be Strings for the other
         // operation
         validateOperands(expression.operator, left, right);
 
-        // TODO: Handle binary operator for operands. Keep in mind, at this point, we know they are doubles, but you
-        // TODO: still need to cast them to doubles. Use the primitive type, e.g. (double)left
+        // TODO: Handle binary operator for operands. Keep in mind, at this point, we
+        // know they are doubles, but you
+        // TODO: still need to cast them to doubles. Use the primitive type, e.g.
+        // (double)left
         // TODO: we do not support >, >=, <, or <= on Strings
-        switch(expression.operator.type) {
+        switch (expression.operator.type) {
             case SUBTRACT:
                 return null;
             case MULTIPLY:
@@ -93,30 +109,39 @@ public class SpartieInterpreter {
     private boolean isEquivalent(Object left, Object right) {
         // They are equal under the following conditions:
         // 1. They are both null
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
         // 2. Their values are the same
-
-        return false;
+        return left.equals(right);
     }
 
     // TODO: Complete implementation of isTrue.
     private boolean isTrue(Object object) {
         // We should return false if an object is null
-        // If an object is of type boolean, we should return the primitive equivalent of that value
-
+        if (object == null)
+            return false;
+        // If an object is of type boolean, we should return the primitive equivalent of
+        // that value
+        if (object instanceof Boolean)
+            return (boolean) object;
+        // All other values should return true
         return true;
     }
 
     // Validate the type
     private void validateOperand(Token operator, Object operand) {
-        if (operand instanceof Double) return;
+        if (operand instanceof Double)
+            return;
         error("Invalid type on line " + operator.line + " : " + operator.text + operand);
     }
 
     private void validateOperands(Token operator, Object operand1, Object operand2) {
-        if (operand1 instanceof Double && operand2 instanceof Double) return;
+        if (operand1 instanceof Double && operand2 instanceof Double)
+            return;
         error("Invalid type on line " + operator.line + " : " + operand1 + operator.text + operand2);
     }
-
 
     private void error(String message) {
         System.err.println(message);
