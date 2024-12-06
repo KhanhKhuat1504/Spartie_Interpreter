ebd2169ecf6fd511622ab34f0b1ec5c37b6afa1a
KhanhKhuat1504 <86178396+KhanhKhuat1504@users.noreply.github.com>
2024-12-06 17:52:37 -0500

    Finish Environment

--- Spartie/Spartie/src/Environment.java
+++ Spartie/Spartie/src/Environment.java
@@ -19,20 +19,33 @@ public class Environment {
     }
 
     Object get(String name) {
-        // TODO: Return variable if it exists in our current environment, otherwise, check enclosing, otherwise,
-        //  return null (it does not exist)
-
-        return null;
+        // TODO: Return variable if it exists in our current environment, otherwise,
+        // check enclosing, otherwise,
+        // return null (it does not exist)
+        if (variables.containsKey(name)) {
+            return variables.get(name);
+        } else if (enclosing != null) {
+            return enclosing.get(name);
+        } else {
+            return null;
+        }
     }
 
     // Assign - Replace the value of an existing variable
     void assign(Token name, Object value) {
         // TODO: If the variable exists, then we can assign, otherwise we have an error
-
-        // TODO: If we don't have it in our current environment, try assigning in the enclosing environment
-
+        if (variables.containsKey(name.text)) {
+            variables.put(name.text, value);
+        }
+        // TODO: If we don't have it in our current environment, try assigning in the
+        // enclosing environment
+        else if (enclosing != null) {
+            enclosing.assign(name, value);
+        }
         // Exit on error if we get this far since the variable is undefined
-        System.err.println("Undefined variable: " + name.text);
-        System.exit(ErrorCode.INTERPRET_ERROR);
+        else {
+            System.err.println("Undefined variable: " + name.text);
+            System.exit(ErrorCode.INTERPRET_ERROR);
+        }
     }
 }
